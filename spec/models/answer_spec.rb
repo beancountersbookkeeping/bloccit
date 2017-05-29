de18bf613e1801ffcebeed494d926535023f8a95
Kaylin Leland <kay@thebeancountersbookkeeping.com>
2017-05-29 21:20:39 +0000

    Checkpoint15'

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:question) { Question.new(title: "new question", body: "new body", resolved: false) }
+  let(:answer) { Answer.new(question: question, body: "new answer body") } 
+  
+  it "should respond to body" do 
+      expect(answer).to respond_to(:body)
+  end
 end
