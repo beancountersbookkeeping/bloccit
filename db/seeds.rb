6b37cb000e03ab8697b73786408da674ca8649fa
Kaylin Leland <kay@thebeancountersbookkeeping.com>
2017-05-30 20:33:02 +0000

    Checkpoint16

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+ require 'random_data'
+
+ # Create Posts
+ 50.times do
+   Post.create!(
+     title:  RandomData.random_sentence,
+     body:   RandomData.random_paragraph
+   )
+ end
+ posts = Post.all
+ 
+ # Create Comments
+ 100.times do
+   Comment.create!(
+     post: posts.sample,
+     body: RandomData.random_paragraph
+   )
+ end
+ 
+ Post.find_or_create_by(title: "A Happy Ending", body: "And they lived happily ever after")
+ Comment.find_or_create_by(title: "A Happy Ending", body: "yes")
+ 
+ puts "Seed finished"
+ puts "#{Post.count} posts created"
+ puts "#{Comment.count} comments created"
