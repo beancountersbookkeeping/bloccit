da13413868d4c219d48620c93a048506e0a86ae4
Kaylin Leland <kay@thebeancountersbookkeeping.com>
2017-06-17 20:20:19 +0000

    Checkpoint24

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
     resources :sponsored_posts, except: [:index]
   end
+  
+  resources :users, only: [:new, :create]
+  post 'users/confirm' => 'users#confirm'
 
   get 'about' => 'welcome#about'
   
